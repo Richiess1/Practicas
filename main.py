5e05b5c3483c7e2d05bb3855cca252d4f20da8c6
astridayala <astriayala06@gmail.com>
2025-05-17 09:40:14 -0600

    COMMIT

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 import WhatsappNotification
 import SmsNotification
 import EmailNotification 
+
+def controller():
+    email = EmailNotification("Ricardo","ricardo@gmail.com")
+    whatsApp = WhatsappNotification("Ricardo", "5623-5623","503")
+    sms = SmsNotification("Ricardo", "5623-5623", "503")
+
+    email.send("Enviado desde correo")
+    whatsApp.send("Enviado desde WhatsApp")
+    sms.send("Enviado desde SMS")
+
+controller()
+
